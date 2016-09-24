e9e25bd764ad233479860d5795d94012fc0a3d70
shakiba charkhkar <shakiba charkhkar>
2016-09-24 13:13:08 +0330

    first commit

---
+++ form.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+var querystring = require('querystring');
+
+
+var server = http.createServer(function (req, res) {
+    if(req.method == 'GET'){
+
+        res.writeHead(200,{"Content-Type":"text/html"});
+        fs.createReadStream("index.html","UTF-8").pipe(res);
+
+    }
+    else if (req.method== 'POST'){
+
+        var string = "";
+        req.on('data',function(information){
+            string += information;
+        });
+        req.on('end',function(){
+            var data = ((querystring.parse(string)));
+
+
+
+            res.writeHead(200,{"Content-Type":"application/json"});
+            res.end(JSON.stringify(data));
+
+        });
+
+    }
+});
+
+
+server.listen(8888);
+console.log("server listening on 8888");
